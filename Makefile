d0c72eafcd22c07e02a3944185eb319eb5ce361b
kigi <kigi.chang@gmail.com>
2021-09-24 22:28:00 +0800

    [fix] typo

--- Makefile
+++ Makefile
@@ -5,10 +5,10 @@ GOPATH=${shell echo ${HOME}}/go2
 PKG=github.com/kigichang/goscala
 
 test:
-	env GOROOT=${GOORT} GOPATH=${GOPATH} ${GOROOT}/bin/go test -v -cover -gcflags -G=3 .
-	env GOROOT=${GOORT} GOPATH=${GOPATH} ${GOROOT}/bin/go test -v -cover -gcflags -G=3 ${PKG}/monad
-	env GOROOT=${GOORT} GOPATH=${GOPATH} ${GOROOT}/bin/go test -v -cover -gcflags -G=3 ${PKG}/either
-	env GOROOT=${GOORT} GOPATH=${GOPATH} ${GOROOT}/bin/go test -v -cover -gcflags -G=3 ${PKG}/opt
+	env GOROOT=${GOROOT} GOPATH=${GOPATH} ${GOROOT}/bin/go test -v -cover -gcflags -G=3 .
+	env GOROOT=${GOROOT} GOPATH=${GOPATH} ${GOROOT}/bin/go test -v -cover -gcflags -G=3 ${PKG}/monad
+	env GOROOT=${GOROOT} GOPATH=${GOPATH} ${GOROOT}/bin/go test -v -cover -gcflags -G=3 ${PKG}/either
+	env GOROOT=${GOROOT} GOPATH=${GOPATH} ${GOROOT}/bin/go test -v -cover -gcflags -G=3 ${PKG}/opt
 
 tidy:
-	env GOROOT=${GOORT} GOPATH=${GOPATH} ${GOROOT}/bin/go mod tidy
+	env GOROOT=${GOROOT} GOPATH=${GOPATH} ${GOROOT}/bin/go mod tidy
--- monad/monadm.go
+++ monad/monadm.go
@@ -1,25 +1,27 @@
 package monad
 
 func MFold[T, C, U any](fetch func() (T, C), fa func(C) U, fb func(T) U) U {
-	
-		var v T
-		var c C
-		v, c = fetch()
-		
-		var x interface{} = c
-		ok := false
-		switch xv := x.(type) {
-		case bool:
-			ok = xv
-		default:
-			ok = (xv == nil)
-		}
 
-		if ok {
-			return fb(v)
-		}
-		return fa(c)
-	
+	var v T
+	var c C
+	v, c = fetch()
+
+	var x interface{} = c
+	ok := false
+	switch xv := x.(type) {
+	case bool:
+		ok = xv
+	case error:
+		ok = false
+	default:
+		ok = (xv == nil)
+	}
+
+	if ok {
+		return fb(v)
+	}
+	return fa(c)
+
 }
 
 func MFoldBool[T, U any](fetch func() (T, bool), z func() U, f func(T) U) U {
@@ -30,11 +32,7 @@ func MFoldBool[T, U any](fetch func() (T, bool), z func() U, f func(T) U) U {
 }
 
 func MFoldErr[T, U any](fetch func() (T, error), z func(error) U, f func(T) U) U {
-	v, err := fetch()
-	if err == nil {
-		return f(v)
-	}
-	return z(err)
+	return MFold[T, error, U](fetch, z, f)
 }
 
 func MFoldLeft[T, U any](s []T, z U, fn func(a U, b T) U) U {
@@ -46,7 +44,7 @@ func MFoldLeft[T, U any](s []T, z U, fn func(a U, b T) U) U {
 }
 
 func MScanLeft[T, U any](s []T, z U, fn func(U, T) U) []U {
-	return MFoldLeft[T, []U](s, []U{z}, func (a []U, b T) []U {
+	return MFoldLeft[T, []U](s, []U{z}, func(a []U, b T) []U {
 		return append(a, fn(a[len(a)-1], b))
 	})
 }
@@ -54,16 +52,16 @@ func MScanLeft[T, U any](s []T, z U, fn func(U, T) U) []U {
 func MFoldRight[T, U any](s []T, z U, fn func(T, U) U) U {
 	zz := z
 	size := len(s)
-	for i := size -1; i >= 0; i-- {
+	for i := size - 1; i >= 0; i-- {
 		zz = fn(s[i], zz)
 	}
 	return zz
 }
 
 func MScanRight[T, U any](s []T, z U, fn func(T, U) U) []U {
-	
+
 	result := MFoldRight[T, []U](s, []U{z}, func(a T, b []U) []U {
-		return append(b, fn(a, b[len(b) - 1]))
+		return append(b, fn(a, b[len(b)-1]))
 	})
 
 	size := len(result)
@@ -71,18 +69,18 @@ func MScanRight[T, U any](s []T, z U, fn func(T, U) U) []U {
 
 	for i := 0; i < half; i++ {
 		tmp := result[i]
-		result[i] = result[size - 1 - i] 
-		result[size - 1 - i] = tmp
+		result[i] = result[size-1-i]
+		result[size-1-i] = tmp
 	}
 
 	return result
 }
 
 func MMap[T, U any](s []T, fn func(T) U) []U {
-	return MFoldLeft[T, []U](s, []U{}, func (z []U, a T) []U {
+	return MFoldLeft[T, []U](s, []U{}, func(z []U, a T) []U {
 		z = append(z, fn(a))
 		return z
-	}) 
+	})
 }
 
 func MFlatMap[T, U any](s []T, fn func(T) []U) []U {
@@ -90,7 +88,7 @@ func MFlatMap[T, U any](s []T, fn func(T) []U) []U {
 		z = append(z, fn(a)...)
 		return z
 	})
-	
+
 }
 
 func MFuncCompose[T, A, R any](f func(T) R, g func(A) T) func(A) R {
@@ -111,7 +109,7 @@ func MFuncUnitAndThen[R, U any](f func() R, g func(R) U) func() U {
 	}
 }
 
-func MFuncBoolAndThen[T, R, U any](f func(T) (R, bool), g func(R, bool) U) func (T) U {
+func MFuncBoolAndThen[T, R, U any](f func(T) (R, bool), g func(R, bool) U) func(T) U {
 	return func(v T) U {
 		return g(f(v))
 	}
@@ -127,8 +125,8 @@ func Currying2[A, B, C any](f func(A, B) C) func(A) func(B) C {
 
 func Currying3[A, B, C, D any](f func(A, B, C) D) func(A) func(B) func(C) D {
 	return func(a A) func(B) func(C) D {
-		return func (b B) func (C) D {
-			return func (c C) D {
+		return func(b B) func(C) D {
+			return func(c C) D {
 				return f(a, b, c)
 			}
 		}
