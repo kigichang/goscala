56f1bc274c5f85e18dbd5177f2b15aa3e6d948be
kigi <kigi.chang@leadtek.io>
2021-09-24 17:48:38 +0800

    [wip] method and currying

--- monad/monad.go
+++ monad/monad.go
@@ -1,122 +1,53 @@
 package monad
 
 func Fold[T, C, U any](fetch func() (T, C)) func(func(C) U, func(T) U) U {
-	
-	return func(z func(C) U, f func(T) U) U {
-		return MFold(fetch, z, f)
-	}
+	return Currying3To2(MFold[T, C, U])(fetch)
 }
 
 func FoldBool[T, U any](fetch func() (T, bool)) func(func() U, func(T) U) U {
-	return func(z func() U, f func(T) U) U {
-		return MFoldBool(fetch, z, f)
-	}
+	return Currying3To2(MFoldBool[T, U])(fetch)
 }
 
 func FoldErr[T, U any](fetch func() (T, error)) func(func(error) U, func(T) U) U {
-	return func(z func(error) U, f func(T) U) U {
-		return MFoldErr(fetch, z, f)
-	}
+	return Currying3To2(MFoldErr[T, U])(fetch)
 }
 
 func FoldLeft[T, U any](s []T) func(U) func(func(U, T) U) U {
-	return func(z U) func(func(U, T) U) U {
-		return func (fn func(a U, b T) U) U {
-			return MFoldLeft(s, z, fn)
-		}
-	}
+	return Currying3(MFoldLeft[T, U])(s)
 }
 
 func ScanLeft[T, U any](s []T) func(U) func(func(U, T) U) []U {
-	return func(z U) func(func(U, T) U) []U {
-		return func(fn func(U, T) U) []U {
-			return FoldLeft[T, []U](s)([]U{z})(func (a []U, b T) []U {
-				return append(a, fn(a[len(a)-1], b))
-			})
-		}
-	}
+	return Currying3(MScanLeft[T, U])(s)
 }
 
 func FoldRight[T, U any](s []T) func(U) func(func(T, U) U) U {
-	return func (z U) func(func(T, U) U) U {
-		return func(fn func(T, U) U) U {
-			zz := z
-			size := len(s)
-			for i := size -1; i >= 0; i-- {
-				zz = fn(s[i], zz)
-			}
-			return zz
-		}
-	}
+	return Currying3(MFoldRight[T, U])(s)
 }
 
 func ScanRight[T, U any](s []T) func(U) func(func(T, U) U) []U {
-	return func(z U) func(func(T, U) U) []U {
-		return func (fn func(T, U) U) []U {
-			result := FoldRight[T, []U](s)([]U{z})(func(a T, b []U) []U {
-				return append(b, fn(a, b[len(b) - 1]))
-			})
-
-			size := len(result)
-			half := size / 2
-
-			for i := 0; i < half; i++ {
-				tmp := result[i]
-				result[i] = result[size - 1 - i] 
-				result[size - 1 - i] = tmp
-			}
-
-			return result
-		}
-	}
+	return Currying3(MScanRight[T, U])(s)
 }
 
 func Map[T, U any](s []T) func(func(T) U) []U {
-	return func(fn func(T) U) []U {
-		return FoldLeft[T, []U](s)([]U{})(func (z []U, a T) []U {
-			z = append(z, fn(a))
-			return z
-		}) 
-	}
+	return Currying2(MMap[T, U])(s)
 }
 
 func FlatMap[T, U any](s []T) func(func(T) []U) []U {
-	return func(fn func(T) []U) []U {
-		return FoldLeft[T, []U](s)([]U{})(func(z []U, a T) []U {
-			z = append(z, fn(a)...)
-			return z
-		})
-	}
+	return Currying2(MFlatMap[T, U])(s)
 }
 
 func FuncCompose[T, A, R any](f func(T) R) func(func(A) T) func(A) R {
-	return func(g func(A) T) func(A) R {
-		return func(v A) R {
-			return f(g(v))
-		}
-	}
+	return Currying2(MFuncCompose[T, A, R])(f)
 }
 
 func FuncAndThen[T, R, U any](f func(T) R) func(func(R) U) func(T) U {
-	return func(g func(R) U) func(T) U {
-		return func(v T) U {
-			return g(f(v))
-		}
-	}
+	return Currying2(MFuncAndThen[T, R, U])(f)
 }
 
 func FuncUnitAndThen[R, U any](f func() R) func(func(R) U) func() U {
-	return func(g func(R) U) func() U {
-		return func() U {
-			return g(f())
-		}
-	}
+	return Currying2(MFuncUnitAndThen[R, U])(f)
 }
 
 func FuncBoolAndThen[T, R, U any](f func(T) (R, bool)) func(func (R, bool) U) func (T) U {
-	return func(g func(R, bool) U) func(T) U {
-		return func(v T) U {
-			return g(f(v))
-		}
-	}
+	return Currying2(MFuncBoolAndThen[T, R, U])(f)
 }
--- monad/monadm.go
+++ monad/monadm.go
@@ -44,97 +44,101 @@ func MFoldLeft[T, U any](s []T, z U, fn func(a U, b T) U) U {
 	}
 	return zz
 }
-//
-//func ScanLeft[T, U any](s []T) func(U) func(func(U, T) U) []U {
-//	return func(z U) func(func(U, T) U) []U {
-//		return func(fn func(U, T) U) []U {
-//			return FoldLeft[T, []U](s)([]U{z})(func (a []U, b T) []U {
-//				return append(a, fn(a[len(a)-1], b))
-//			})
-//		}
-//	}
-//}
-//
-//func FoldRight[T, U any](s []T) func(U) func(func(T, U) U) U {
-//	return func (z U) func(func(T, U) U) U {
-//		return func(fn func(T, U) U) U {
-//			zz := z
-//			size := len(s)
-//			for i := size -1; i >= 0; i-- {
-//				zz = fn(s[i], zz)
-//			}
-//			return zz
-//		}
-//	}
-//}
-//
-//func ScanRight[T, U any](s []T) func(U) func(func(T, U) U) []U {
-//	return func(z U) func(func(T, U) U) []U {
-//		return func (fn func(T, U) U) []U {
-//			result := FoldRight[T, []U](s)([]U{z})(func(a T, b []U) []U {
-//				return append(b, fn(a, b[len(b) - 1]))
-//			})
-//
-//			size := len(result)
-//			half := size / 2
-//
-//			for i := 0; i < half; i++ {
-//				tmp := result[i]
-//				result[i] = result[size - 1 - i] 
-//				result[size - 1 - i] = tmp
-//			}
-//
-//			return result
-//		}
-//	}
-//}
-//
-//func Map[T, U any](s []T) func(func(T) U) []U {
-//	return func(fn func(T) U) []U {
-//		return FoldLeft[T, []U](s)([]U{})(func (z []U, a T) []U {
-//			z = append(z, fn(a))
-//			return z
-//		}) 
-//	}
-//}
-//
-//func FlatMap[T, U any](s []T) func(func(T) []U) []U {
-//	return func(fn func(T) []U) []U {
-//		return FoldLeft[T, []U](s)([]U{})(func(z []U, a T) []U {
-//			z = append(z, fn(a)...)
-//			return z
-//		})
-//	}
-//}
-//
-//func FuncCompose[T, A, R any](f func(T) R) func(func(A) T) func(A) R {
-//	return func(g func(A) T) func(A) R {
-//		return func(v A) R {
-//			return f(g(v))
-//		}
-//	}
-//}
-//
-//func FuncAndThen[T, R, U any](f func(T) R) func(func(R) U) func(T) U {
-//	return func(g func(R) U) func(T) U {
-//		return func(v T) U {
-//			return g(f(v))
-//		}
-//	}
-//}
-//
-//func FuncUnitAndThen[R, U any](f func() R) func(func(R) U) func() U {
-//	return func(g func(R) U) func() U {
-//		return func() U {
-//			return g(f())
-//		}
-//	}
-//}
-//
-//func FuncBoolAndThen[T, R, U any](f func(T) (R, bool)) func(func (R, bool) U) func (T) U {
-//	return func(g func(R, bool) U) func(T) U {
-//		return func(v T) U {
-//			return g(f(v))
-//		}
-//	}
-//}
+
+func MScanLeft[T, U any](s []T, z U, fn func(U, T) U) []U {
+	return MFoldLeft[T, []U](s, []U{z}, func (a []U, b T) []U {
+		return append(a, fn(a[len(a)-1], b))
+	})
+}
+
+func MFoldRight[T, U any](s []T, z U, fn func(T, U) U) U {
+	zz := z
+	size := len(s)
+	for i := size -1; i >= 0; i-- {
+		zz = fn(s[i], zz)
+	}
+	return zz
+}
+
+func MScanRight[T, U any](s []T, z U, fn func(T, U) U) []U {
+	
+	result := FoldRight[T, []U](s)([]U{z})(func(a T, b []U) []U {
+		return append(b, fn(a, b[len(b) - 1]))
+	})
+
+	size := len(result)
+	half := size / 2
+
+	for i := 0; i < half; i++ {
+		tmp := result[i]
+		result[i] = result[size - 1 - i] 
+		result[size - 1 - i] = tmp
+	}
+
+	return result
+}
+
+func MMap[T, U any](s []T, fn func(T) U) []U {
+	return MFoldLeft[T, []U](s, []U{}, func (z []U, a T) []U {
+		z = append(z, fn(a))
+		return z
+	}) 
+}
+
+func MFlatMap[T, U any](s []T, fn func(T) []U) []U {
+	return MFoldLeft[T, []U](s, []U{}, func(z []U, a T) []U {
+		z = append(z, fn(a)...)
+		return z
+	})
+	
+}
+
+func MFuncCompose[T, A, R any](f func(T) R, g func(A) T) func(A) R {
+	return func(v A) R {
+		return f(g(v))
+	}
+}
+
+func MFuncAndThen[T, R, U any](f func(T) R, g func(R) U) func(T) U {
+	return func(v T) U {
+		return g(f(v))
+	}
+}
+
+func MFuncUnitAndThen[R, U any](f func() R, g func(R) U) func() U {
+	return func() U {
+		return g(f())
+	}
+}
+
+func MFuncBoolAndThen[T, R, U any](f func(T) (R, bool), g func(R, bool) U) func (T) U {
+	return func(v T) U {
+		return g(f(v))
+	}
+}
+
+func Currying2[A, B, C any](f func(A, B) C) func(A) func(B) C {
+	return func(a A) func(B) C {
+		return func(b B) C {
+			return f(a, b)
+		}
+	}
+}
+
+func Currying3[A, B, C, D any](f func(A, B, C) D) func(A) func(B) func(C) D {
+	return func(a A) func(B) func(C) D {
+		return func (b B) func (C) D {
+			return func (c C) D {
+				return f(a, b, c)
+			}
+		}
+	}
+}
+
+func Currying3To2[A, B, C, D any](f func(A, B, C) D) func(A) func(B, C) D {
+	return func(a A) func(B, C) D {
+		return func(b B, c C) D {
+			return f(a, b, c)
+		}
+	}
+}
