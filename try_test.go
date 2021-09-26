d4e813b4fd099b265d4115af7778106332b8fb17
kigi <kigi.chang@gmail.com>
2021-09-26 16:25:56 +0800

    [test] try

--- try_test.go
+++ try_test.go
@@ -10,22 +10,26 @@ import (
 
 func TestSuccess(t *testing.T) {
 	v := 0
-	try := goscala.Success[int](v)
-	t.Log(try)
-	assert.Equal(t, true, try.IsSuccess())
-	assert.Equal(t, false, try.IsFailure())
-	assert.Equal(t, v, try.Get())
-	assert.Equal(t, goscala.ErrUnsupported, try.Failed())
+	tr := goscala.Success[int](v)
+	t.Log(tr)
+	assert.Equal(t, true, tr.IsSuccess())
+	assert.Equal(t, false, tr.IsFailure())
+	assert.Equal(t, v, tr.Get())
+	assert.Equal(t, goscala.ErrUnsupported, tr.Failed())
+	assert.Equal(t, v, tr.Success())
+	v2, ok := tr.Fetch()
+	assert.Equal(t, v, v2)
+	assert.True(t, ok)
 }
 
 func TestFailure(t *testing.T) {
 	assert.Panics(t, func() { goscala.Failure[int](nil) })
 
 	testErr := fmt.Errorf("test failure")
-	try := goscala.Failure[int](testErr)
-	t.Log(try)
-	assert.Equal(t, false, try.IsSuccess())
-	assert.Equal(t, true, try.IsFailure())
-	assert.Equal(t, testErr, try.Failed())
-	assert.Panics(t, func() { try.Get() })
+	tr := goscala.Failure[int](testErr)
+	t.Log(tr)
+	assert.Equal(t, false, tr.IsSuccess())
+	assert.Equal(t, true, tr.IsFailure())
+	assert.Equal(t, testErr, tr.Failed())
+	assert.Panics(t, func() { tr.Get() })
 }
