2665574b31d78353a20501e61fc7c542d49c201b
kigi <kigi.chang@gmail.com>
2021-10-01 23:00:09 +0800

    [test] either test

--- either/either_test.go
+++ either/either_test.go
@@ -9,10 +9,31 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/kigichang/goscala"
+	gs "github.com/kigichang/goscala"
 	"github.com/kigichang/goscala/either"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestBool(t *testing.T) {
+	e := either.Bool(1, true)
+
+	assert.True(t, e.IsRight())
+	assert.Equal(t, 1, e.Right())
+
+	e = either.Bool(1, false)
+	assert.True(t, e.IsLeft())
+	assert.Equal(t, false, e.Left())
+}
+
+func TestErr(t *testing.T) {
+	e := either.Err(1, nil)
+	assert.True(t, e.IsRight())
+	assert.Equal(t, 1, e.Right())
+
+	e = either.Err(1, gs.ErrEmpty)
+	assert.True(t, e.IsLeft())
+	assert.Equal(t, gs.ErrEmpty, e.Left())
+}
 
 func TestCond(t *testing.T) {
 	lv := 100
@@ -30,28 +51,28 @@ func TestCond(t *testing.T) {
 }
 
 func TestFlatMap(t *testing.T) {
-	f := func(v string) goscala.Either[int, int64] {
+	f := func(v string) gs.Either[int, int64] {
 		a, err := strconv.ParseInt(v, 10, 64)
 		if err != nil {
-			return goscala.Left[int, int64](0)
+			return gs.Left[int, int64](0)
 		}
-		return goscala.Right[int, int64](a)
+		return gs.Right[int, int64](a)
 	}
 
-	r := goscala.Right[int, string]("1000")
+	r := gs.Right[int, string]("1000")
 
 	e := either.FlatMap(r, f)
 
 	assert.Equal(t, true, e.IsRight())
 	assert.Equal(t, int64(1000), e.Right())
 
-	r = goscala.Right[int, string]("abc")
+	r = gs.Right[int, string]("abc")
 	e = either.FlatMap(r, f)
 	assert.Equal(t, false, e.IsRight())
 	assert.Equal(t, true, e.IsLeft())
 	assert.Equal(t, 0, e.Left())
 
-	l := goscala.Left[int, string](100)
+	l := gs.Left[int, string](100)
 	e = either.FlatMap(l, f)
 
 	assert.Equal(t, true, e.IsLeft())
@@ -71,15 +92,15 @@ func TestFold(t *testing.T) {
 		return a * 10
 	}
 
-	r := goscala.Right[int, string]("1000")
+	r := gs.Right[int, string]("1000")
 	e := either.Fold(r, fa, fb)
 	assert.Equal(t, int64(1000*10), e)
 
-	r = goscala.Right[int, string]("abc")
+	r = gs.Right[int, string]("abc")
 	e = either.Fold(r, fa, fb)
 	assert.Equal(t, int64(-1), e)
 
-	l := goscala.Left[int, string](100)
+	l := gs.Left[int, string](100)
 	e = either.Fold(l, fa, fb)
 	assert.Equal(t, int64(100+10), e)
 }
@@ -93,11 +114,11 @@ func TestMap(t *testing.T) {
 		return "flower"
 	}
 
-	e := either.Map(goscala.Right[int, int](12), f)
+	e := either.Map(gs.Right[int, int](12), f)
 	assert.Equal(t, true, e.IsRight())
 	assert.Equal(t, "flower", e.Right())
 
-	e = either.Map(goscala.Left[int, int](12), f)
+	e = either.Map(gs.Left[int, int](12), f)
 	assert.Equal(t, true, e.IsLeft())
 	assert.Equal(t, 12, e.Left())
 }
