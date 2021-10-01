96dafd8bffbf53748606f1dade144c962a966e63
kigi <kigi.chang@gmail.com>
2021-10-01 23:16:12 +0800

    [docs] add doc.go

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Copyright © 2021 Kigi Chang <kigi.chang@gmail.com>
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+/*
+	GoScala combines Go features and Scala monads.
+*/
+package goscala
