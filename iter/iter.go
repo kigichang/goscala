885cc6c5dd24fe29ea2e455eead0f9ced8e9db68
kigi <kigi.chang@gmail.com>
2021-10-02 11:48:58 +0800

    [feat] iterator

--- iter/iter.go
+++ iter/iter.go
@@ -32,7 +32,7 @@ func newAbstractIter[T, U any](i Iter[T], fn func(T) U) Iter[U] {
 	}
 }
 
-func GenIter[T any](s ...T) Iter[T] {
+func Gen[T any](s ...T) Iter[T] {
 	idx := -1
 	ss := &s
 	return &abstractIter[T]{
@@ -54,12 +54,51 @@ func Map[T, U any](a Iter[T], fn func(T) U) Iter[U] {
 	return newAbstractIter[T, U](a, fn)
 }
 
+func FlatMap[T, U any](a Iter[T], fn func(T) Iter[U]) Iter[U] {
+	var cur Iter[U] = &abstractIter[U]{
+		next: func() bool {
+			return false
+		},
+		get: func() (ret U) {
+			return
+		},
+	}
+	return &abstractIter[U]{
+		next: func() bool {
+			if !cur.Next() {
+				if !a.Next() {
+					return false
+				}
+				cur = fn(a.Get())
+				return cur.Next()
+			}
+			return true
+		},
+		get: func() U {
+			return cur.Get()
+		},
+	}
+}
+
 func Slice[T any](a Iter[T]) []T {
 	ret := []T{}
 	for a.Next() {
 		ret = append(ret, a.Get())
 	}
 	return ret
+}
+
+func Reverse[T any](a Iter[T]) Iter[T] {
+	dst := Slice(a)
+	size := len(dst)
+	half := size / 2
+	end := size - 1
+	for i := 0; i < half; i++ {
+		tmp := dst[i]
+		dst[i] = dst[end-i]
+		dst[end-i] = tmp
+	}
+	return Gen(dst...)
 }
 
 func FoldLeft[T, U any](a Iter[T], z U, fn func(U, T) U) U {
@@ -69,4 +108,36 @@ func FoldLeft[T, U any](a Iter[T], z U, fn func(U, T) U) U {
 		zz = fn(zz, a.Get())
 	}
 	return zz
+}
+
+func FoldRight[T, U any](a Iter[T], z U, fn func(T, U) U) U {
+	iter := Reverse(a)
+	return FoldLeft(iter, z, func(a U, b T) U {
+		return fn(b, a)
+	})
+}
+
+func ScanLeft[T, U any](a Iter[T], z U, fn func(U, T) U) Iter[U] {
+	zz := z
+	first := true
+	return &abstractIter[U]{
+		next: func() (ok bool) {
+			return first || a.Next()
+		},
+		get: func() U {
+			if first {
+				first = false
+				return zz
+			}
+			zz = fn(zz, a.Get())
+			return zz
+		},
+	}
+}
+
+func ScanRight[T, U any](a Iter[T], z U, fn func(T, U) U) Iter[U] {
+	it := Reverse(a)
+	return Reverse(ScanLeft(it, z, func(a U, b T) U {
+		return fn(b, a)
+	}))
 }
--- iter/iter_test.go
+++ iter/iter_test.go
@@ -14,7 +14,7 @@ import (
 
 func TestGenIter(t *testing.T) {
 
-	it := iter.GenIter(1, 2, 3, 4)
+	it := iter.Gen(1, 2, 3, 4)
 
 	ss := iter.Slice(it)
 
@@ -24,15 +24,71 @@ func TestGenIter(t *testing.T) {
 func TestFoldLeft(t *testing.T) {
 	src := []int{1, 3, 5, 7, 9, 2, 4, 6, 8}
 
-	it := iter.GenIter(src...)
+	it := iter.Gen(src...)
 	fn1 := func(v1, v2 int) int {
 		return v1 - v2
 	}
 	assert.Equal(t, -45, iter.FoldLeft(it, 0, fn1))
 
-	it = iter.GenIter(src...)
+	it = iter.Gen(src...)
 	fn2 := func(v1, v2 int) int {
 		return v1 + v2
 	}
 	assert.Equal(t, 45, iter.FoldLeft(it, 0, fn2))
+}
+
+func TestFoldRight(t *testing.T) {
+	src := []int{1, 3, 5, 7, 9, 2, 4, 6, 8}
+
+	it := iter.Gen(src...)
+	fn1 := func(v1, v2 int) int {
+		return v1 - v2
+	}
+	assert.Equal(t, 9, iter.FoldRight(it, 0, fn1))
+
+	it = iter.Gen(src...)
+	fn2 := func(v1, v2 int) int {
+		return v1 + v2
+	}
+	assert.Equal(t, 45, iter.FoldRight(it, 0, fn2))
+}
+
+func TestScanLeft(t *testing.T) {
+	src := []int{1, 3, 5, 7, 9, 2, 4, 6, 8}
+
+	fn := func(v1, v2 int) int {
+		return v1 + v2
+	}
+
+	dst := iter.Slice(iter.ScanLeft(iter.Gen(src...), 100, fn))
+	ans := []int{100, 101, 104, 109, 116, 125, 127, 131, 137, 145}
+	assert.Equal(t, ans, dst)
+}
+
+func TestScanRight(t *testing.T) {
+	src := []int{1, 3, 5, 7, 9, 2, 4, 6, 8}
+	fn := func(v1, v2 int) int {
+		return v1 + v2
+	}
+
+	dst := iter.Slice(iter.ScanRight(iter.Gen(src...), 100, fn))
+	ans := []int{145, 144, 141, 136, 129, 120, 118, 114, 108, 100}
+	assert.Equal(t, ans, dst)
+}
+
+func TestFlatMapAndMap(t *testing.T) {
+	s1 := []int{1, 2, 3}
+	s2 := []int{4, 5, 6}
+
+	dst := iter.FlatMap(
+		iter.Gen(s1...),
+		func(x int) iter.Iter[int] {
+			return iter.Map(iter.Gen(s2...), func(y int) int {
+				return x * y
+			})
+		},
+	)
+
+	ans := []int{4, 5, 6, 8, 10, 12, 12, 15, 18}
+	assert.Equal(t, ans, iter.Slice(dst))
 }
