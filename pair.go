9b46a54ae0fd7f6899ed37c9aa9d559e0fa3d130
kigi <kigi.chang@gmail.com>
2021-10-14 20:59:12 +0800

    [refactor] rename _pair to pair

--- pair.go
+++ pair.go
@@ -11,27 +11,27 @@ type Pair[K comparable, V any] interface {
 	Get() (K, V)
 }
 
-type _pair[K comparable, V any] struct {
+type pair[K comparable, V any] struct {
 	key K
 	val V
 }
 
-var _ Pair[int, int] = &_pair[int, int]{}
+var _ Pair[int, int] = &pair[int, int]{}
 
-func (p *_pair[K, V]) Key() K {
+func (p *pair[K, V]) Key() K {
 	return p.key
 }
 
-func (p *_pair[K, V]) Value() V {
+func (p *pair[K, V]) Value() V {
 	return p.val
 }
 
-func (p *_pair[K, V]) Get() (K, V) {
+func (p *pair[K, V]) Get() (K, V) {
 	return p.key, p.val
 }
 
 func P[K comparable, V any](k K, v V) Pair[K, V] {
-	return &_pair[K, V]{
+	return &pair[K, V]{
 		key: k,
 		val: v,
 	}
